1529494b7f6abc6965ae94080fee6a056a5e0b5a
Brittany Plummer <brittanyplummer@gmail.com>
2021-06-28 18:32:36 -0400

    fixing deployment issue

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 function App() {
   return (
  
-   <Router>
+   <Router basename= {process.env.PUBLIC_URL}>
       <div className="app">
           <Switch>
           <Route path="/" exact component= { About }/>
